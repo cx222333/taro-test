0fa36150bc4591359d209e3f44baab67e75f5813
chenxiao <chenxiao@shoyoo.com>
2020-10-14 09:31:58 +0800

    Map 设置经纬度demo

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,16 +1,42 @@
-import React from "react";
-import { View } from "@tarojs/components";
+import React, { useEffect, useReducer } from 'react'
+import { Map } from '@tarojs/components'
+import Taro, { getLocation } from '@tarojs/taro'
 
-import "./index.scss";
+import './index.scss'
 
 const Test = () => {
-  return process.env.TARO_ENV === "weapp" ? (
-    <test-weapp />
-  ) : process.env.TARO_ENV === "alipay" ? (
-    <test-dingtalk />
-  ) : (
-    <View>ohter</View>
-  );
-};
+    const [{ latitude, longitude, polygons }, dispatch] = useReducer((state, data) => ({ ...state, ...data }), {
+        latitude: 0,
+        longitude: 0,
+        polygons: []
+    })
 
-export default Test;
+    useEffect(() => {
+        getLocation().then(res => {
+            const { latitude: lat, longitude: lng } = res
+
+            Taro.showToast({ title: `latitude: ${lat}, longitude: ${lng}`, icon: 'none' })
+
+            const offset = 0.5
+            const list = [
+                {
+                    points: [
+                        { latitude: lat - offset, longitude: lng - offset },
+                        { latitude: lat - offset, longitude: lng + offset },
+                        { latitude: lat + offset, longitude: lng + offset },
+                        { latitude: lat + offset, longitude: lng - offset }
+                    ],
+                    color: '#fc5c1ddd',
+                    strokeColor: '#fc5c1ddd',
+                    fillColor: '#fc5c1d33'
+                }
+            ]
+
+            dispatch({ latitude: lat, longitude: lng, polygons: list })
+        })
+    }, [])
+
+    return <Map latitude={latitude} longitude={longitude} polygons={polygons} />
+}
+
+export default Test
